1b2cc3cc26ca1589d0f0f7299f1f47533d2ed628
Beatriz Corrêa <bcorreacontato@gmail.com>
2025-04-03 19:50:37 -0300

    commit primeiro front

---
+++ .assets/script.js
@@ -0,0 +1,22 @@
+
+/*
+por tag: getElementByName()
+por id: getElementById ()
+por nome: getEleemntByName()
+por classe: getElementByClassName()
+por seletor: querySelector()
+
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('input#email')
+let assunto = document.querySelector('assunto')
+
+nome.style.width ='100%'
+email.style.width = '100%'
+
+function validaNome(){
+    if (nome.value.length){
+
+    }
+}
